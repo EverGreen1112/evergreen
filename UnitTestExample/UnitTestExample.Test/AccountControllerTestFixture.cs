95ddfa5b9ba4722aa960517ea871a7f27840f651
kosztelp <peter.kosztel@stud.uni-corvinus.hu>
2026-04-07 22:13:03 +0200

    "E-mail validálás teszt kész"

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        [TestCase("abcd1234", false)]             // Jelszó e-mail helyett
+        [TestCase("irf@uni-corvinus", false)]     // Hiányzó domain
+        [TestCase("irf.uni-corvinus.hu", false)]  // Hiányzó @ jel
+        [TestCase("irf@uni-corvinus.hu", true)]   // Helyes e-mail
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange - Előkészítés
+            var accountController = new AccountController();
+
+            // Act - Cselekvés
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert - Ellenőrzés
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ UnitTestExample/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("440e3340-2b94-4ccd-9f8f-e2d89867c0e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
